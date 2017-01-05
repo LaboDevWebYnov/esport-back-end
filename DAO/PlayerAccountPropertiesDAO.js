227752fa3bff70d6335d9deff33a1b7ad6b3e20b
AntoineMaitre <ant.maitre@gmail.com>
2017-01-06 00:41:56 +0100

    [swagger][PlayerAccountProperties] added spec of PlayerAccountPropertiesDAO for handling the player accounts properties related to the corresponding games or plateforms APIs

---
+++ DAO/PlayerAccountPropertiesDAO.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Antoine on 06/01/2017.
+ */
+//todo GET playerAccount/properties/ --properties of all playerAccounts
+//todo GET playerAccount/{playerAccountId}/properties/ --properties of an account
+//todo GET playerAccount/{playerAccountId}/properties/{key} --properties of an playerAccount by key
+//todo GET playerAccount/{playerAccountId}/properties/{value} --properties of an playerAccount by value
+
+//todo POST playerAccount/{playerAccountId}/addproperty/ + body --add property-ies to a playerAccount
+//todo PUT playerAccount/{playerAccountId}/updateproperty/ + body --updates given property-ies key(s)'s value(s) of a playerAccount
+//todo DELETE playerAccount/{playerAccountId}/removeProperty/{key} --remove property-ies from a playerAccount
