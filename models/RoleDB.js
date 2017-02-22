a7c85f80997afff0b23f5f04135ea950507dd4ad
amaitre <antoine.maitre@ynov.com>
2017-02-22 12:27:11 +0100

    [teams] added role schema for player account role handling

---
+++ models/RoleDB.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Antoine on 22/02/2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Role = new Schema({
+    playerAccount: {type: Schema.ObjectId, ref: 'PlayerAccount', required: true},
+    role: {type: String, required: true},
+    team: {type: Schema.ObjectId, ref: 'Team', required: true},
+    active: {type: Boolean, required: true, default: true},
+    created_at: {type: Date, required: true, default: new Date()},
+    updated_at: {type: Date, required: true, default: new Date()}
+});
+
+Role.pre('save', function (next) {
+    var now = new Date();
+    this.updated_at = now;
+    if (!this.created_at) {
+        this.created_at = now;
+    }
+    next();
+});
+
+Role.pre('update', function (next) {
+    this.update({}, {$set: {updated_at: new Date()}});
+    next();
+});
+
+Role.pre('findOneAndUpdate', function (next) {
+    this.update({}, {$set: {updated_at: new Date()}});
+    next();
+});
+
+exports.Role = mongoose.model('Role', Role);
